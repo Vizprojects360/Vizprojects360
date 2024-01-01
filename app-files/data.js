1123691ab70ce2ad75b4a7b5e37e042fee65ad92
Vizprojects360 <155273263+Vizprojects360@users.noreply.github.com>
2024-01-01 23:09:58 +0100

    Upload

---
+++ app-files/data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010088",
+      "name": "R0010088",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 0.9159994655199792,
+        "pitch": 0,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.157120649231725,
+          "pitch": 0.5679231748730675,
+          "rotation": 0,
+          "target": "1-r0010089"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010089",
+      "name": "R0010089",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.42239762582998885,
+          "pitch": 0.6584072819356699,
+          "rotation": 0,
+          "target": "0-r0010088"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
